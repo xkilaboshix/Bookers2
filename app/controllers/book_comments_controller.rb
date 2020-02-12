1a1dc2380c379d157c2936c1e4421651309c7aee
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-02-12 12:41:55 +0000

    いいね機能とコメント機能を非同期通信に

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.user_id = current_user.id
     comment.save
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
   end
   def destroy
-    BookComment.destroy(params[:id])
-    redirect_back(fallback_location: books_path)
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    # redirect_back(fallback_location: books_path)
   end
   private
   def book_comment_params
